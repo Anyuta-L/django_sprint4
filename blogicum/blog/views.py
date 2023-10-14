0c703ab62489e5cfad3410c2ece7a9152efce498
Anyuta-L <a.laptinskaia@gmail.com>
2023-10-14 21:38:39 +0300

    new2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,10 +5,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models import Count
+
+
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
+
+
 from django.utils import timezone
+
 from django.views.generic import (CreateView, DeleteView, DetailView, 
                                   UpdateView, View)
 
